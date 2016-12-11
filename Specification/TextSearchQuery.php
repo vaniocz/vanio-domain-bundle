8b2e2d032e9c68a683095a071d9c807560af7d2b
maryo <mario.dweller@seznam.cz>
2016-12-11 13:42:36 +0100

    Removed text search configuration setup from TextSearchQuery

--- Specification/TextSearchQuery.php
+++ Specification/TextSearchQuery.php
@@ -6,9 +6,6 @@ use Happyr\DoctrineSpecification\Filter\Filter;
 
 class TextSearchQuery extends TextSearchSpecification implements Filter
 {
-    /** @var bool */
-    private static $searchConfigurationSetUp = false;
-
     /** @var string */
     private $searchTerm;
 
@@ -35,10 +32,6 @@ class TextSearchQuery extends TextSearchSpecification implements Filter
      */
     public function getFilter(QueryBuilder $queryBuilder, $dqlAlias): string
     {
-        if (!self::$searchConfigurationSetUp) {
-            $this->setUpSearchConfiguration($queryBuilder);
-        }
-
         $queryBuilder->setParameter('_searchQuery', $this->processSearchTerm($this->searchTerm));
 
         return sprintf(
@@ -46,20 +39,5 @@ class TextSearchQuery extends TextSearchSpecification implements Filter
             $this->dqlAlias ?? $dqlAlias,
             $this->searchDocumentField
         );
-    }
-
-    /**
-     * @param QueryBuilder $queryBuilder
-     */
-    private function setUpSearchConfiguration(QueryBuilder $queryBuilder)
-    {
-        /*$db = $queryBuilder->getEntityManager()->getConnection();
-        if (!$db->fetchColumn("SELECT true FROM pg_ts_dict WHERE dictname = 'cs'")) {
-            $db->query('CREATE TEXT SEARCH DICTIONARY cs (template=ispell, dictfile = czech, afffile=czech, stopwords=czech)');
-            $db->query('CREATE TEXT SEARCH CONFIGURATION ru (copy=english)');
-            $db->query('ALTER TEXT SEARCH CONFIGURATION ru ALTER MAPPING FOR word, asciiword WITH cs, simple');
-        }*/
-
-        self::$searchConfigurationSetUp = true;
     }
 }
