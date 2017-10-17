5fcdf3026916e69bece8b8163d1413c1781be56b
Marek Štípek <mario.dweller@seznam.cz>
2017-10-17 14:17:51 +0200

    Fixed GetPostParamConverter merging parameters even when id was present in route

--- Request/GetPostParamConverter.php
+++ Request/GetPostParamConverter.php
@@ -17,46 +17,30 @@ class GetPostParamConverter implements ParamConverterInterface
         $name = $configuration->getName();
         $class = $configuration->getClass();
         $options = $configuration->getOptions();
-
         $attributes = $request->attributes->all();
         $parameters = $request->query->all() + $request->request->all();
 
-        if ($request->attributes->has($name)) {
+        if (array_key_exists($name, $attributes)) {
             return;
-        }
-
-        if ($class === null) {
+        } elseif ($class === null) {
             if (array_key_exists($name, $parameters)) {
                 $attributes += [$name => $parameters[$name]];
             }
         } else {
-            if (isset($options['mapping'])) {   // set parameters from mapping
-                foreach (array_keys($options['mapping']) as $param) {
-                    if (!isset($options['exclude']) || !in_array($param, $options['exclude'])) {
-                        if (array_key_exists($param, $parameters)) {
-                            $attributes += [$param => $parameters[$param]];
-                        }
-                    }
-                }
+            if (isset($options['mapping'])) {
+                $attributes += array_diff_key(
+                    array_intersect_key($parameters, $options['mapping']),
+                    array_flip($options['exclude'] ?? [])
+                );
             } elseif (isset($options['id'])) {
-                $id = $options['id'];
-                if (is_array($id)) {    // set ID parameters (array)
-                    foreach ($id as $field) {
-                        if (array_key_exists($field, $parameters)) {
-                            $attributes += [$field => $parameters[$field]];
-                        }
-                    }
-                } else {  // set ID parameter
-                    if (array_key_exists($id, $parameters)) {
-                        $attributes += [$id => $parameters[$id]];
-                    }
-                }
-            } elseif (array_key_exists($name, $parameters)) {   // set parameter with same name
+                $attributes += array_intersect_key($parameters, array_flip((array) $options['id']));
+            } elseif (!array_key_exists('id', $attributes) && array_key_exists($name, $parameters)) {
                 $attributes += [$name => $parameters[$name]];
-            } elseif (array_key_exists('id', $parameters)) { // fallback to parameter 'id'
+            } elseif (array_key_exists('id', $parameters)) {
                 $attributes += ['id' => $parameters['id']];
             }
         }
+
         $request->attributes->replace($attributes);
     }
 }
