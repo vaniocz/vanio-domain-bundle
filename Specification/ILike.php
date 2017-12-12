dbac8f2880228d2862eef14f7734a50ebc940c74
davidv <david.vondrak@vanio.cz>
2017-12-12 16:59:02 +0100

    Added specification for ILike operator.

---
+++ Specification/ILike.php
@@ -0,0 +1,26 @@
+<?php
+namespace Vanio\DomainBundle\Specification;
+
+use Doctrine\ORM\Query\Expr\Comparison;
+use Doctrine\ORM\QueryBuilder;
+use Happyr\DoctrineSpecification\Filter\Like;
+use Happyr\DoctrineSpecification\ValueConverter;
+
+class ILike extends Like
+{
+    public function getFilter(QueryBuilder $qb, $dqlAlias)
+    {
+        if ($this->dqlAlias !== null) {
+            $dqlAlias = $this->dqlAlias;
+        }
+
+        $paramName = $this->getParameterName($qb);
+        $qb->setParameter($paramName, ValueConverter::convertToDatabaseValue($this->value, $qb));
+
+        return (string) new Comparison(
+            sprintf('LOWER(%s.%s)', $dqlAlias, $this->field),
+            'LIKE',
+            sprintf(':%s', strtolower($paramName))
+        );
+    }
+}
