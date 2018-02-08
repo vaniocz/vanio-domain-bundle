9229a6d43a709309e128d974c96e5cdfc320316d
davidv <david.vondrak@vanio.cz>
2018-02-08 20:12:40 +0100

    Added CurrentLocale specification.

---
+++ Specification/CurrentLocale.php
@@ -0,0 +1,64 @@
+<?php
+namespace Vanio\DomainBundle\Specification;
+
+use Doctrine\ORM\QueryBuilder;
+use Happyr\DoctrineSpecification\Query\QueryModifier;
+use Vanio\DomainBundle\Translatable\TranslatableListener;
+
+class CurrentLocale implements QueryModifier
+{
+    /**
+     * @var bool
+     */
+    private $withUntranslated;
+
+    /**
+     * @var string|null
+     */
+    private $dqlAlias;
+
+    public function __construct(bool $withUntranslated = false, $dqlAlias = null)
+    {
+        $this->withUntranslated = $withUntranslated;
+        $this->dqlAlias = $dqlAlias;
+    }
+
+    public function withUntranslated(): self
+    {
+        return new self(true, $this->dqlAlias);
+    }
+
+    public function modify(QueryBuilder $queryBuilder, $dqlAlias)
+    {
+        if ($this->dqlAlias !== null) {
+            $this->dqlAlias = $dqlAlias;
+        }
+
+        $currentLocale = $this->resolveCurrentLocale($queryBuilder);
+        if (!$currentLocale) {
+            return;
+        }
+
+        $queryBuilder
+            ->leftJoin("$dqlAlias.translations", '__t', 'WITH', '__t.locale = :locale')
+            ->setParameter('locale', $currentLocale)
+            ->addSelect('__t');
+
+        if (!$this->withUntranslated) {
+            $queryBuilder->where('__t.locale IS NOT NULL');
+        }
+    }
+
+    private function resolveCurrentLocale(QueryBuilder $queryBuilder): ?string
+    {
+        foreach ($queryBuilder->getEntityManager()->getEventManager()->getListeners() as $listeners) {
+            foreach ($listeners as $listener) {
+                if ($listener instanceof TranslatableListener) {
+                    return $listener->resolveCurrentLocale();
+                }
+            }
+        }
+
+        return null;
+    }
+}
