87738bb6e604a0ef2493304577680287c99b8629
Marek Štípek <mario.dweller@seznam.cz>
2018-02-13 01:26:50 +0100

    Changed JsonbArrayType to JsonbType

---
+++ Doctrine/Types/JsonbType.php
@@ -0,0 +1,42 @@
+<?php
+namespace Vanio\DomainBundle\Doctrine\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+
+class JsonbType extends Type
+{
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform): string
+    {
+        return 'JSONB';
+    }
+
+    /**
+     * @param mixed $value
+     * @param AbstractPlatform $platform
+     * @return string|null
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return $value === null ? null :  json_encode($value);
+    }
+
+    /**
+     * @param mixed $value
+     * @param AbstractPlatform $platform
+     * @return mixed
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if ($value === null || $value === '') {
+            return null;
+        }
+
+        return json_decode($value, true);
+    }
+
+    public function getName(): string
+    {
+        return 'jsonb';
+    }
+}
