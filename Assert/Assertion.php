8e77795e61be03d53cf0e1adca4db5e089aff3a6
Marek Štípek <mario.dweller@seznam.cz>
2018-02-16 18:47:52 +0100

    Made ::inArray not strict

--- Assert/Assertion.php
+++ Assert/Assertion.php
@@ -2,21 +2,47 @@
 namespace Vanio\DomainBundle\Assert;
 
 use Assert\Assertion as BaseAssertion;
-use Assert\InvalidArgumentException;
+use Assert\AssertionFailedException;
 
 class Assertion extends BaseAssertion
 {
     /**
-     * Asserts that value is an existing image file.
-     *
      * @param mixed $value
-     * @param string|null $message
+     * @param array $values
+     * @param string|callable|null $message
      * @param string|null $propertyPath
-     * @throws InvalidArgumentException
+     * @return bool
+     * @throws AssertionFailedException
      */
-    public static function supportedImageFile($value, string $message = null, string $propertyPath = null)
+    public static function inArray($value, array $values, $message = null, $propertyPath = null): bool
+    {
+        if (!in_array($value, $values, true)) {
+            $message = sprintf(
+                static::generateMessage($message ?: 'Value "%s" is not an element of the valid values: %s'),
+                static::stringify($value),
+                implode(', ', array_map([get_called_class(), 'stringify'], $values))
+            );
+
+            throw static::createException($value, $message, static::INVALID_VALUE_IN_ARRAY, $propertyPath, [
+                'values' => $values,
+            ]);
+        }
+
+        return true;
+    }
+
+    /**
+     * @param mixed $value
+     * @param string|callable|null $message
+     * @param string|null $propertyPath
+     * @return bool
+     * @throws AssertionFailedException
+     */
+    public static function supportedImageFile($value, $message = null, $propertyPath = null): bool
     {
         static::file((string) $value);
         static::notEmpty(@getimagesize($value)[0], $message ?: 'Unknown image format.', $propertyPath);
+
+        return true;
     }
 }
