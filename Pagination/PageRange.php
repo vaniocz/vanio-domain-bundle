1e36bb659eb7fbfc3c4913914d8a51ed4ac76f62
dvondrak <david.vondrak@ongr.io>
2018-02-23 20:19:05 +0100

    Added possibility to set different number of records on first page fo… (#15)
    Added possibility to set different number of records on first page for paginator.

--- Pagination/PageRange.php
+++ Pagination/PageRange.php
@@ -16,20 +16,24 @@ class PageRange implements PageSpecification
     /** @var int */
     private $recordsPerPage;
 
-    public function __construct(int $fromPage, int $toPage, int $recordsPerPage)
+    /** @var int */
+    private $recordsOnFirstPage;
+
+    public function __construct(int $fromPage, int $toPage, int $recordsPerPage, int $recordsOnFirstPage = null)
     {
         $this->fromPage = max($fromPage, 1);
         $this->toPage = max($toPage, $this->fromPage);
         $this->recordsPerPage = max($recordsPerPage, 1);
+        $this->recordsOnFirstPage = $recordsOnFirstPage ?? $recordsPerPage;
     }
 
-    public static function create(string $value, int $recordsPerPage): PageSpecification
+    public static function create(string $value, int $recordsPerPage, int $recordsOnFirstPage = null): PageSpecification
     {
         list($fromPage, $toPage) = explode('-', $value) + [null, null];
         $fromPage = ctype_digit($fromPage) ? (int) $fromPage : 1;
         $toPage = ctype_digit($toPage) ? (int) $toPage : $fromPage;
 
-        return new self($fromPage, $toPage, $recordsPerPage);
+        return new self($fromPage, $toPage, $recordsPerPage, $recordsOnFirstPage);
     }
 
     public function fromPage(): int
@@ -47,14 +51,19 @@ class PageRange implements PageSpecification
         return $this->recordsPerPage;
     }
 
+    public function recordsOnFirstPage(): int
+    {
+        return $this->recordsOnFirstPage;
+    }
+
     public function firstRecord(): int
     {
-        return $this->recordsPerPage * ($this->fromPage - 1);
+        return $this->fromPage === 1 ? 0 : ($this->fromPage - 2) * $this->recordsPerPage + $this->recordsOnFirstPage;
     }
 
     public function lastRecord(): int
     {
-        return $this->toPage * $this->recordsPerPage;
+        return $this->toPage * $this->recordsPerPage + $this->recordsOnFirstPage - $this->recordsPerPage;
     }
 
     /**
