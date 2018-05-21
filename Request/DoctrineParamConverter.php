d7b7f3c6c79a5480d9d4a6c7a47030763869d2e7
maryo <mario.dweller@seznam.cz>
2018-05-21 17:24:41 +0200

    Better exception messages for APIs

--- Request/DoctrineParamConverter.php
+++ Request/DoctrineParamConverter.php
@@ -6,6 +6,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\DoctrineParamConverter as BaseDoctrineParamConverter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Vanio\Stdlib\Strings;
 
 class DoctrineParamConverter extends BaseDoctrineParamConverter
 {
@@ -13,8 +15,14 @@ class DoctrineParamConverter extends BaseDoctrineParamConverter
     {
         try {
             return parent::apply($request, $configuration);
+        } catch (NotFoundHttpException $e) {
+            throw new NotFoundHttpException(sprintf('%s not found.', Strings::baseName($configuration->getClass())));
         } catch (ConversionException $e) {
-            throw new BadRequestHttpException($e->getMessage(), $e);
+            preg_match('~Could not convert database value "(.+)" to Doctrine Type (.+)~', $e->getMessage(), $matches);
+
+            throw $matches
+                ? new BadRequestHttpException(sprintf('Invalid %s value "%s".', $matches[2], $matches[1]), $e)
+                : $e;
         }
     }
 }
