a791bfe41d99631dcfb57448fe408c7e326f8eed
dvondrak <david.vondrak@ongr.io>
2018-06-08 23:02:27 +0200

    Added method for adding foreign key into ForeignKeysHelper. (#21)
    Added method for adding foreign key into ForeignKeysHelper.

--- Doctrine/ForeignKeysHelper.php
+++ Doctrine/ForeignKeysHelper.php
@@ -16,13 +16,36 @@ class ForeignKeysHelper
     }
 
     /**
+     * @param string $localTableName
+     * @param string[]|string $localColumnNames
+     * @param string $foreignTableName
+     * @param string[]|string $foreignColumnNames
+     * @param array $options
+     * @param string|null $foreignKeyName
+     */
+    public function addTableForeignKey(
+        string $localTableName,
+        $localColumnNames,
+        string $foreignTableName,
+        $foreignColumnNames,
+        array $options,
+        string $foreignKeyName = null
+    ): void {
+        $localColumnNames = (array) $localColumnNames;
+        $foreignColumnNames = (array) $foreignColumnNames;
+        $foreignKeyName = $foreignKeyName ?? sprintf('fk_%s_%s', $localTableName, implode('_', $localColumnNames));
+        $foreignKey = new ForeignKeyConstraint($localColumnNames, $foreignTableName, $foreignColumnNames, $foreignKeyName, $options);
+        $this->schemaManager->createForeignKey($foreignKey, $this->schemaManager->getDatabasePlatform()->quoteSingleIdentifier($localTableName));
+    }
+
+    /**
      * @param string $table
-     * @param string[] $columns
+     * @param string[]|string $columns
      * @param array $options
      */
-    public function updateTableForeignKeyOptions(string $table, array $columns, array $options)
+    public function updateTableForeignKeyOptions(string $table, $columns, array $options)
     {
-        $foreignKey = $this->getTableForeignKey($table, $columns);
+        $foreignKey = $this->getTableForeignKey($table, (array) $columns);
         $foreignKey = new ForeignKeyConstraint(
             $foreignKey->getLocalColumns(),
             $foreignKey->getForeignTableName(),
@@ -30,7 +53,7 @@ class ForeignKeysHelper
             $foreignKey->getName(),
             $options + $foreignKey->getOptions()
         );
-        $this->schemaManager->dropAndCreateForeignKey($foreignKey, $table);
+        $this->schemaManager->dropAndCreateForeignKey($foreignKey, $this->schemaManager->getDatabasePlatform()->quoteSingleIdentifier($table));
     }
 
     /**
