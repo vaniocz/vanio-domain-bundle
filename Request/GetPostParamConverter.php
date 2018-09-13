fb88c74e5274701efa9f43fe98e8e6bf338f6037
Marek Štípek <mario.dweller@seznam.cz>
2018-09-13 04:21:37 +0200

    GetPostParamConverter refactoring and explanation * fixing of bug where route parameters were replaced by get/post parameters

--- Request/GetPostParamConverter.php
+++ Request/GetPostParamConverter.php
@@ -1,6 +1,7 @@
 <?php
 namespace Vanio\DomainBundle\Request;
 
+use BeSimple\I18nRoutingBundle\Routing\Loader\AnnotatedRouteControllerLoader;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
@@ -17,17 +18,22 @@ class GetPostParamConverter implements ParamConverterInterface
     /** @var ConfigurableRequirementsInterface|null */
     private $configurableUrlGenerator;
 
+    /** @var AnnotatedRouteControllerLoader|null */
+    private $annotatedRouteControllerLoader;
+
     /** @var ManagerRegistry|null */
     private $registry;
 
     public function __construct(
         UrlGeneratorInterface $urlGenerator,
         UrlGeneratorInterface $defaultUrlGenerator = null,
+        AnnotatedRouteControllerLoader $annotatedRouteControllerLoader = null,
         ManagerRegistry $registry = null
     ) {
         $this->urlGenerator = $urlGenerator;
         $this->configurableUrlGenerator = $this->resolveConfigurableRequirementsUrlGenerator($urlGenerator)
             ?: $this->resolveConfigurableRequirementsUrlGenerator($defaultUrlGenerator);
+        $this->annotatedRouteControllerLoader = $annotatedRouteControllerLoader;
         $this->registry = $registry;
     }
 
@@ -47,27 +53,27 @@ class GetPostParamConverter implements ParamConverterInterface
         $class = $configuration->getClass();
         $options = $configuration->getOptions();
         $attributes = $request->attributes->all();
-        $parameters = $request->query->all() + $request->request->all();
-        $additionalAttributes = $this->resolveAdditionalAttributes($attributes);
+        $routeParameters = $this->resolveRouteParameters($attributes);
+        $parameters = array_intersect_key($attributes, $routeParameters)
+            + $request->query->all()
+            + $request->request->all();
 
-        if (array_key_exists($name, $attributes) && !isset($additionalAttributes[$name])) {
-            return;
-        } elseif (!$class || !$this->isEntityClass($class, $options)) {
+        if (!$class || !$this->isEntityClass($class, $options)) {
             if (array_key_exists($name, $parameters)) {
-                $attributes = [$name => $parameters[$name]] + $attributes;
+                $attributes[$name] = $parameters[$name];
             }
         } else {
             if (isset($options['mapping'])) {
-                $attributes += array_diff_key(
+                $attributes = array_diff_key(
                     array_intersect_key($parameters, $options['mapping']),
                     array_flip($options['exclude'] ?? [])
-                );
+                ) + $attributes;
             } elseif (isset($options['id'])) {
                 $attributes = array_intersect_key($parameters, array_flip((array) $options['id'])) + $attributes;
-            } elseif (!array_key_exists('id', $attributes) && array_key_exists($name, $parameters)) {
-                $attributes = [$name => $parameters[$name]] + $attributes;
+            } elseif (!isset($routeParameters['id']) && array_key_exists($name, $parameters)) {
+                $attributes[$name] = $parameters[$name];
             } elseif (array_key_exists('id', $parameters)) {
-                $attributes = ['id' => $parameters['id']] + $attributes;
+                $attributes['id'] = $parameters['id'];
             }
         }
 
@@ -89,14 +95,29 @@ class GetPostParamConverter implements ParamConverterInterface
         return null;
     }
 
-    private function resolveAdditionalAttributes(array $attributes): array
+    /**
+     * Returns an array of route parameters which placeholders arepresent in route definition.
+     * @param mixed[] $attributes
+     * @return string[]
+     */
+    private function resolveRouteParameters(array $attributes): array
     {
         if (!isset($attributes['_route'])) {
             return [];
         }
 
-        $additionalAttributes = [];
-        $routeParameters = array_fill_keys(array_keys($attributes['_route_params']), '__UNIQUE_DEFAULT_VALUE__');
+        $routeParameters = array_keys($attributes['_route_params']);
+        $routeParameters = array_combine($routeParameters, $routeParameters);
+
+        if (!$this->annotatedRouteControllerLoader) {
+            return $routeParameters;
+        }
+
+        // AnnotatedRouteControllerLoader from BeSimpleI18nRoutingBundle loads routes differently than the default one from Symfony.
+        // It populates _route_params with all default parameter values from controller action even with ones which placeholders are not present in route definition.
+        // The following workaround relies on the fact that URL generator generates URL with parameters which placeholders are not present in route definition as query string.
+        // @see https://github.com/BeSimple/BeSimpleI18nRoutingBundle/blob/83d2cf7c9ba6e6e3caed5d063b185ae54645eeee/src/Routing/Loader/AnnotatedRouteControllerLoader.php#L43
+        // @see https://github.com/symfony/symfony/blob/8ab7077225eadee444ba76c83c667688763c56fb/src/Symfony/Component/Routing/Loader/AnnotationClassLoader.php#L202
 
         if ($this->configurableUrlGenerator) {
             $isStrictRequirements = $this->configurableUrlGenerator->isStrictRequirements();
@@ -111,7 +132,7 @@ class GetPostParamConverter implements ParamConverterInterface
 
         parse_str(parse_url($url, PHP_URL_QUERY), $additionalAttributes);
 
-        return $additionalAttributes;
+        return array_diff_key($routeParameters, $additionalAttributes);
     }
 
     private function isEntityClass(string $class, array $options): bool
