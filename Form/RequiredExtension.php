85feb225186c68ac724e3dc43833b10b9a871853
Marek Štípek <mario.dweller@seznam.cz>
2018-10-13 21:07:13 +0200

    * Simplified RequiredExtension

--- Form/RequiredExtension.php
+++ Form/RequiredExtension.php
@@ -3,33 +3,34 @@ namespace Vanio\DomainBundle\Form;
 
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Validator\Constraints\FormValidator;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormConfigBuilder;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\PropertyAccess\PropertyPath;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Valid;
-use Symfony\Component\Validator\Mapping\ClassMetadata;
-use Symfony\Component\Validator\Mapping\Factory\MetadataFactoryInterface;
-use Vanio\Stdlib\Objects;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class RequiredExtension extends AbstractTypeExtension
 {
-    /** @var MetadataFactoryInterface */
-    private $metadataFactory;
+    /** @var ValidatorInterface */
+    private $validator;
 
-    public function __construct(MetadataFactoryInterface $metadataFactory)
+    /** @var FormInterface[] */
+    private $forms = [];
+
+    public function __construct(ValidatorInterface $validator)
     {
-        $this->metadataFactory = $metadataFactory;
+        $this->validator = $validator;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->addEventListener(FormEvents::POST_SET_DATA, [$this, 'onPostSetData']);
+        $builder->addEventListener(FormEvents::POST_SUBMIT, [$this, 'onPostSubmit']);
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -51,37 +52,75 @@ class RequiredExtension extends AbstractTypeExtension
     /**
      * @internal
      */
-    public function onPostSetData(FormEvent $event)
+    public function onPostSubmit(FormEvent $event)
     {
         $form = $event->getForm();
-        $groups = $this->resolveValidationGroups($form);
 
-        if (!$form->getConfig() instanceof FormConfigBuilder || !$this->shouldValidateRequired($form, $groups)) {
+        if ($this->shouldValidateRequired($form)) {
+            $this->forms[] = $form;
+        }
+
+        if (!$form->isRoot()) {
             return;
         }
 
-        $options = $form->getConfig()->getOptions();
-        $options['constraints'][] = new NotBlank([
-            'message' => $form->getConfig()->getOption('required_message'),
-            'groups' => $groups,
-        ]);
-        Objects::setPropertyValue($form->getConfig(), 'options', $options, FormConfigBuilder::class);
+        array_walk($this->forms, [$this, 'validateRequired']);
+        $this->forms = [];
     }
 
     /**
-     * @return string[]
+     * @internal
      */
-    private function resolveValidationGroups(FormInterface $form): array
+    public function validateRequired(FormInterface $form)
     {
-        $resolveValidationGroups = function () use ($form) {
-            return FormValidator::{'getValidationGroups'}($form);
-        };
-        $resolveValidationGroups = $resolveValidationGroups->bindTo(null, FormValidator::class);
+        if (!$form->isSynchronized()) {
+            return;
+        }
 
-        return $resolveValidationGroups();
+        while ($form->getParent() && $form->getConfig()->getErrorBubbling()) {
+            $form = $form->getParent();
+        }
+
+        if ($this->hasNotBlankOrNotNullViolationError($form)) {
+            return;
+        }
+
+        foreach ($this->validator->validate($form->getData(), new NotBlank) as $violation) {
+            $this->addViolationError($form, $violation);
+        }
     }
 
-    private function shouldValidateRequired(FormInterface $form, array $groups): bool
+    private function addViolationError(FormInterface $form, ConstraintViolationInterface $violation)
+    {
+        $form->addError(new FormError(
+            $violation->getMessage(),
+            $violation->getMessageTemplate(),
+            $violation->getParameters(),
+            $violation->getPlural(),
+            $violation
+        ));
+    }
+
+    private function hasNotBlankOrNotNullViolationError(FormInterface $form): bool
+    {
+        foreach ($form->getErrors() as $error) {
+            $cause = $error->getCause();
+
+            if (!$cause instanceof ConstraintViolation) {
+                continue;
+            }
+
+            $constraint = $cause->getConstraint();
+
+            if ($constraint instanceof NotBlank || $constraint instanceof NotNull) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function shouldValidateRequired(FormInterface $form): bool
     {
         if (!$form->isRequired() || !$this->resolveValidateRequired($form)) {
             return false;
@@ -89,21 +128,19 @@ class RequiredExtension extends AbstractTypeExtension
 
         $config = $form->getConfig();
         $parent = $form->getParent();
-        $skippedParentTypes = ['choice', 'scalar_object', 'repeated'];
+        static $skippedParentTypes;
+
+        if ($skippedParentTypes === null) {
+            $skippedParentTypes = array_flip(['choice', 'scalar_object', 'repeated']);
+        }
 
         if ($config->getCompound() && $config->getType()->getBlockPrefix() !== 'repeated') {
             return false;
-        } elseif ($parent && in_array($parent->getConfig()->getType()->getBlockPrefix(), $skippedParentTypes)) {
-            return false;
-        } elseif ($this->hasNotBlankConstraint($config->getOption('constraints'), $groups)) {
+        } elseif ($parent && isset($skippedParentTypes[$parent->getConfig()->getType()->getBlockPrefix()])) {
             return false;
         }
 
-        return !$parent || !$this->shouldValidateEmbedded($parent) || !$this->hasPropertyNotBlankConstraint(
-            $parent->getConfig()->getDataClass(),
-            $form->getPropertyPath(),
-            $groups
-        );
+        return true;
     }
 
     private function resolveValidateRequired(FormInterface $form): bool
@@ -117,76 +154,5 @@ class RequiredExtension extends AbstractTypeExtension
         } while ($form = $form->getParent());
 
         return false;
-    }
-
-    private function hasNotBlankConstraint(array $constraints, array $groups): bool
-    {
-        foreach ($constraints as $constraint) {
-            if ($constraint instanceof NotBlank && array_intersect($groups, $constraint->groups)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private function shouldValidateEmbedded(FormInterface $form): bool
-    {
-        $dataClass = $form->getConfig()->getDataClass();
-
-        return $dataClass && (
-            !$form->getParent()
-            || $this->hasValidConstraint($form->getConfig()->getOption('constraints'))
-            || $this->hasClassValidConstraint($dataClass)
-        );
-    }
-
-    private function hasValidConstraint(array $constraints): bool
-    {
-        foreach ($constraints as $constraint) {
-            if ($constraint instanceof Valid) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private function hasClassValidConstraint(string $dataClass): bool
-    {
-        $metadata = $this->getMetadataFor($dataClass);
-
-        return $metadata && $this->hasValidConstraint($metadata->getConstraints());
-    }
-
-    /**
-     * @param string $dataClass
-     * @param PropertyPath $propertyPath
-     * @param string[] $groups
-     * @return bool
-     */
-    private function hasPropertyNotBlankConstraint(string $dataClass, PropertyPath $propertyPath, array $groups): bool
-    {
-        if ($metadata = $this->getMetadataFor($dataClass)) {
-            foreach ($propertyPath->getElements() as $element) {
-                $metadata = $metadata->getPropertyMetadata($element);
-            }
-
-            foreach ($metadata as $propertyMetadata) {
-                if ($this->hasNotBlankConstraint($propertyMetadata->getConstraints(), $groups)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * @return ClassMetadata|null
-     */
-    private function getMetadataFor(string $class)
-    {
-        return $this->metadataFactory->hasMetadataFor($class) ? $this->metadataFactory->getMetadataFor($class) : null;
     }
 }
