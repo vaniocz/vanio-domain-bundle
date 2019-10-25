17a77728a06ab0d5697ce6eaef093a53105a6240
davidv <david.vondrak@vanio.cz>
2019-10-25 12:22:38 +0200

    Fixed MultilingualFilterParamConverter.

--- Pagination/MultilingualFilterParamConverter.php
+++ Pagination/MultilingualFilterParamConverter.php
@@ -4,22 +4,27 @@ namespace Vanio\DomainBundle\Pagination;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Translation\TranslatorInterface;
 use Vanio\DomainBundle\Request\LocaleParamConverter;
 use Vanio\DomainBundle\Specification\Locale;
 
 class MultilingualFilterParamConverter implements ParamConverterInterface
 {
+    /** @var TranslatorInterface */
+    private $translator;
+
     /** @var callable|null */
     private $currentLocaleCallable;
 
-    public function __construct(?callable $currentLocaleCallable)
+    public function __construct(TranslatorInterface $translator, ?callable $currentLocaleCallable)
     {
+        $this->translator = $translator;
         $this->currentLocaleCallable = $currentLocaleCallable;
     }
 
     public function apply(Request $request, ParamConverter $configuration): bool
     {
-        (new FilterParamConverter($configuration->getOptions()))->apply($request, $configuration);
+        (new FilterParamConverter($this->translator, $configuration->getOptions()))->apply($request, $configuration);
         $filter = $request->attributes->get($configuration->getName());
 
         (new LocaleParamConverter($this->currentLocaleCallable, 'filterLocale'))->apply($request, $configuration);
