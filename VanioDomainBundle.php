2f6283c3de246c8d377035b678b220c13ec410d2
Marek Štípek <mario.dweller@seznam.cz>
2019-11-08 04:02:38 +0100

    Added UUID caster to Var Dumper

--- VanioDomainBundle.php
+++ VanioDomainBundle.php
@@ -1,17 +1,28 @@
 <?php
 namespace Vanio\DomainBundle;
 
+use Ramsey\Uuid\UuidInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\VarDumper\Cloner\ClonerInterface;
 use Vanio\DomainBundle\DependencyInjection\OverrideDoctrineParamConverterPass;
 use Vanio\DomainBundle\DependencyInjection\OverridePropertyMappingFactoryPass;
 
 class VanioDomainBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
         $container->addCompilerPass(new OverrideDoctrineParamConverterPass);
         $container->addCompilerPass(new OverridePropertyMappingFactoryPass);
+    }
+
+    public function boot(): void
+    {
+        $cloner = $this->container->get('var_dumper.cloner');
+        assert($cloner instanceof ClonerInterface);
+        $cloner->addCasters([UuidInterface::class => function (UuidInterface $uuid) {
+            return ['uuid' => (string) $uuid];
+        }]);
     }
 }
