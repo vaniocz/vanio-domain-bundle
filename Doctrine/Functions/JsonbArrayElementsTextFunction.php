ed5b478589f8f96b4a1f23ac9af287c0107d16f2
davidv <david.vondrak@vanio.cz>
2020-05-28 17:45:32 +0200

    Added DQL function jsonb_array_elements_text.

---
+++ Doctrine/Functions/JsonbArrayElementsTextFunction.php
@@ -0,0 +1,30 @@
+<?php
+namespace Vanio\DomainBundle\Doctrine\Functions;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\AST\Node;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+class JsonbArrayElementsTextFunction extends FunctionNode
+{
+    /** @var Node */
+    private $json;
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->json = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker): string
+    {
+        return sprintf(
+            'jsonb_array_elements_text(%s)',
+            $this->json->dispatch($sqlWalker)
+        );
+    }
+}
